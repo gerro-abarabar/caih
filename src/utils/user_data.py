4a77837b7f5b27408fe744f13db914b814c0df02
Gerro Abarabar <gerrolabarabar@gmail.com>
2026-06-10 01:55:50 +0800

    one last change

--- src/utils/user_data.py
+++ src/utils/user_data.py
@@ -6,7 +6,7 @@ from typing import Any, Dict
 import streamlit as st
 
 DEFAULT_DATA: Dict[str, Any] = {
-    "version": "v1",
+    "version": "v1.2",
     "user": {
         "name": "John Doe",
         "username": "johndoe",
